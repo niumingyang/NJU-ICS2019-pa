9a39125417532d4b47cef5c00d8e82681a9f7753
tracer-ics2019 <tracer@njuics.org>
2019-09-20 14:37:14 +0800

    >  compile
    nmy
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     14:37:14 up  3:38,  1 user,  load average: 0.12, 0.15, 0.16
    85f94cde258b1c39bbc7ee90c1fdf78c66930c87

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -75,6 +75,7 @@ static int cmd_p(char *args) {
 		for (int i = 0; i < 10; ++i) {
 			fscanf(pf, "%d ", &result);
 			fgets(test_expr, 70000, pf);
+			test_expr[strlen(test_expr)-2] = '\0';
 			int cmd_p_ans = expr(test_expr, &succ);
 			if (succ && result != cmd_p_ans)
 				printf("Error %d: expected %d, get %d\n%s\n", cnt_error, result, cmd_p_ans, test_expr);
