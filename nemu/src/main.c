e20e51356a4e2fab251695bc6c373853a056a49c
tracer-ics2019 <tracer@njuics.org>
2019-09-21 09:27:17 +0800

    >  compile
    nmy
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     09:27:17 up 13 min,  1 user,  load average: 0.03, 0.17, 0.17
    a1e42c61ebffdbc0b03638ca70756c954dbe9ab

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,9 +1,39 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+#include "monitor/expr.h"
+
 int init_monitor(int, char *[]);
 void ui_mainloop(int);
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
+
+		/*use some examples to test this function*/
+		FILE *pf;
+		bool succ = 1;
+		pf = fopen("/home/nmy/ics2019/nemu/tools/gen-expr/input", "r");
+		if (pf==NULL) {
+			printf("open failed\n");
+			return 0;
+		}
+		int result = 0, cnt_error = 0;
+		char test_expr[70000];
+		for (int i = 0; i < 10; ++i) {
+			fscanf(pf, "%d ", &result);
+			fgets(test_expr, 70000, pf);
+			test_expr[strlen(test_expr)-1] = '\0';
+			int cmd_p_ans = expr(test_expr, &succ);
+			if (succ && result != cmd_p_ans) {
+				printf("Expression %d: error\nExpected %d, get %d\nExpression: %s\n\n", i, result, cmd_p_ans, test_expr);
+			    cnt_error++;
+			}
+			else printf("Expression %d: no error\nExpected %d, get %d\n\n", i, result, cmd_p_ans);
+		}
+		fclose(pf);
+		if (!cnt_error) printf("Success!!!\n");
+		else printf("%d Errors", cnt_error);
 
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
