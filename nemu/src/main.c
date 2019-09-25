977024ee3386ea5a49ff340018f04cf9f21f632a
tracer-ics2019 <tracer@njuics.org>
2019-09-25 15:53:23 +0800

    >  compile
    nmy
    Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux
     15:53:22 up 6 min,  1 user,  load average: 0.07, 0.31, 0.21
    8b6b8868786ececef1416db0f1764581abafc0a2

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,41 +1,13 @@
 #include <stdio.h>
-#include <stddef.h>
-#include <string.h>
-#include "monitor/expr.h"
 
 int init_monitor(int, char *[]);
 void ui_mainloop(int);
+void expr_test();
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
-
-		/*use some examples to test expression evaluation*/
-		FILE *pf;
-		bool succ = 1;
-		pf = fopen("/home/nmy/ics2019/nemu/tools/gen-expr/input", "r");
-		if (pf==NULL) {
-			printf("open failed\n");
-			return 0;
-		}
-		int result = 0, cnt_error = 0;
-		char test_expr[70000];
-		for (int i = 0; i < 100; ++i) {
-			fscanf(pf, "%d ", &result);
-			fgets(test_expr, 70000, pf);
-			test_expr[strlen(test_expr)-1] = '\0';
-			int cmd_p_ans = expr(test_expr, &succ);
-			if (result != cmd_p_ans||!succ) {
-			  printf("Expression %d: \e[1;31merror\e[0m\nExpected %d, get %d\nExpression: %s\n", 
-					  i, result, cmd_p_ans, test_expr);
-		      cnt_error++;
-			}
-			//else printf("Expression %d: no error\nExpected %d, get %d\n", i, result, cmd_p_ans);//some details
-		}
-		fclose(pf);
-		if (!cnt_error) printf("\e[1;34mExpression evaluation tests passed successfully\e[0m\n");
-		else printf("%d \e[1;31mErrors\e[0m\n", cnt_error);
-
+  expr_test();
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
 
--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -265,3 +265,31 @@ uint32_t expr(char *e, bool *success) {
   }
   return expr_ans;
 }
+
+/*use some examples to test expression evaluation*/
+void expr_test() {
+	FILE *pf;
+	bool succ = 1;
+	pf = fopen("/home/nmy/ics2019/nemu/tools/gen-expr/input", "r");
+	if (pf==NULL) {
+		printf("open failed\n");
+		assert(0);
+	}
+	int result = 0, cnt_error = 0;
+	char test_expr[70000];
+	for (int i = 0; i < 100; ++i) {
+		fscanf(pf, "%d ", &result);
+		fgets(test_expr, 70000, pf);
+		test_expr[strlen(test_expr)-1] = '\0';
+		int cmd_p_ans = expr(test_expr, &succ);
+		if (result != cmd_p_ans||!succ) {
+		  printf("Expression %d: \e[1;31merror\e[0m\nExpected %d, get %d\nExpression: %s\n", 
+				  i, result, cmd_p_ans, test_expr);
+	      cnt_error++;
+		}
+		//else printf("Expression %d: no error\nExpected %d, get %d\n", i, result, cmd_p_ans);//some details
+	}
+	fclose(pf);
+	if (!cnt_error) printf("\e[1;34mExpression evaluation tests passed successfully\e[0m\n");
+	else printf("%d \e[1;31mErrors\e[0m\n", cnt_error);
+}
