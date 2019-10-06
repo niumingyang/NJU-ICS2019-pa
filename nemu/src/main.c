1eb43d3fa4a5077f9b42c3840f96590c1ebde624
tracer-ics2019 <tracer@njuics.org>
2019-10-06 16:12:02 +0800

    >  compile 181860066 nmy Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  16:12:02 up  4:20,  1 user,  load average: 0.43, 0.40, 0.35 5ef6b37022038925e4a011afd5177ec6c029a856

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -6,7 +6,8 @@ void expr_test();
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
-  int is_batch_mode = init_monitor(argc, argv);
+  //int is_batch_mode = init_monitor(argc, argv);
+  int is_batch_mode = 0;
   expr_test();
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
