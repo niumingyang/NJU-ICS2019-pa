b5a0efd9d2b045c5da19833ec5c4b27329b78d3d
tracer-ics2019 <tracer@njuics.org>
2019-10-21 19:05:10 +0800

    >  compile 181860066 nmy Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  19:05:10 up  6:16,  1 user,  load average: 0.49, 0.43, 0.34 24ef617be4f69d1baf5ef2722cd50b66d25786ec

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -7,7 +7,7 @@
  * This is useful when you use the `si' command.
  * You can modify this value as you want.
  */
-#define MAX_INSTR_TO_PRINT 10
+#define MAX_INSTR_TO_PRINT 50
 
 /* restrict the size of log file */
 #define LOG_MAX (1024 * 1024)
