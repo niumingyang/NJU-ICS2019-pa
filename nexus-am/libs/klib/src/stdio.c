173b3dad76e776642f6a6858d17d68261e3ea72f
tracer-ics2019 <tracer@njuics.org>
2019-10-25 14:00:55 +0800

    >  run 181860066 nmy Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  14:00:55 up  3:50,  1 user,  load average: 0.43, 0.36, 0.29 87e9f1bc9481f5d9b86d47c8caa5e8e3a12e504c

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -8,11 +8,49 @@ int printf(const char *fmt, ...) {
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  return 0;
+  int d, len, ret = 0;
+  char c, *s;
+  while (*fmt) 
+    switch (*fmt++) {
+      case 's':{
+        s = va_arg(ap, char *);
+        len = strlen(s);
+        for (int i = 0; i < len; ++i) *out++ = *s++;
+        ret += len;
+        break;
+      }
+      case 'i':
+      case 'd':{
+        d = va_arg(ap, int);
+        char tmp[100];
+        int cnt = 0;
+        while (d) {
+          tmp[cnt++] = '0' + d%10;
+          d /= 10;
+        }
+        ret += cnt;
+        while (cnt--)
+          *out++ = tmp[cnt];
+        break;
+      }
+      case 'c':{
+        c = (char)va_arg(ap, int);
+        *out++ = c;
+        ret++;
+        break;
+      }
+      default: assert(0);
+    }
+  return ret;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  return 0;
+  va_list ap;
+  int ret;
+  va_start(ap, fmt);
+  ret = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
