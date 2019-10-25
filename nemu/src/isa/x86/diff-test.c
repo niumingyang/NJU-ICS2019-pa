334c590db8a70aaa994fbcd5322c60d84f32cd67
tracer-ics2019 <tracer@njuics.org>
2019-10-25 16:29:09 +0800

    >  compile 181860066 nmy Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  16:29:09 up  6:19,  1 user,  load average: 0.42, 0.57, 0.52 b60aa7de59d065e04ae4402dc0770916ca8cc746

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -2,10 +2,9 @@
 #include "monitor/diff-test.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  //if (memcmp(ref_r, &cpu, DIFFTEST_REG_SIZE) == 0)
-  //  return true;
-  //else return false;
-  return false;
+  if (memcmp(ref_r, &cpu, DIFFTEST_REG_SIZE) == 0)
+    return true;
+  else return false;
 }
 
 void isa_difftest_attach(void) {
