f59e3ba8861fe1ea38b90d5683a12531c862af9c
tracer-ics2019 <tracer@njuics.org>
2019-10-27 21:49:44 +0800

    >  run 181860066 nmy Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  21:49:44 up  6:57,  1 user,  load average: 0.67, 0.88, 1.05 7b7f9156f564095f456937205e339fdee23db526

--- nexus-am/apps/dhrystone/dry.c
+++ nexus-am/apps/dhrystone/dry.c
@@ -798,18 +798,18 @@ int main ()
 
     for (Run_Index = 1; Run_Index <= Number_Of_Runs; ++Run_Index)
     {
-
-      Proc_5();
-      Proc_4();
+_putc('1');
+      Proc_5();_putc('2');
+      Proc_4();_putc('3');
 	/* Ch_1_Glob == 'A', Ch_2_Glob == 'B', Bool_Glob == true */
-      Int_1_Loc = 2;_putc('1');
+      Int_1_Loc = 2;
       Int_2_Loc = 3;
       strcpy (Str_2_Loc, "DHRYSTONE PROGRAM, 2'ND STRING");
       Enum_Loc = Ident_2;
       Bool_Glob = ! Func_2 (Str_1_Loc, Str_2_Loc);
 	/* Bool_Glob == 1 */
       while (Int_1_Loc < Int_2_Loc)  /* loop body executed once */
-      {_putc('0');
+      {
 	Int_3_Loc = 5 * Int_1_Loc - Int_2_Loc;
 	  /* Int_3_Loc == 7 */
 	Proc_7 (Int_1_Loc, Int_2_Loc, &Int_3_Loc);
