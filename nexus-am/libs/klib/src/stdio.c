5a7b90e00c8701620420fa34246cd1456ed0a3a1
tracer-ics2019 <tracer@njuics.org>
2019-10-27 14:20:34 +0800

    >  compile 181860066 nmy Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  14:20:34 up  2:30,  1 user,  load average: 0.42, 0.40, 0.33 66d068914e3104a21ce4ddf1f650f492beb00387

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,30 +3,46 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-  return 0;
+enum {Printf, Vsprintf};
+
+void func_op(char *s1, const char s2, int func_num) {
+  switch (func_num) {
+    case Printf:   _putc(*s2);
+    case Vsprintf: *s1 = s2; break;
+    default: assert(0);
+  }
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
+int fmtop(char *out, const char *fmt, va_list ap, int func_num) {
   char *str;
-  int len, d;
+  int len, d, ret = 0;
   char* s;
+  char c;
   char d_num[30];
   for (str = out; *fmt; ++fmt) {
     if (*fmt != '%') {
-      *str++ = *fmt;
+      func_op(str, *fmt, func_num);
+      if (str != NULL) str++;
+      ret++;
       continue;
     }
     fmt++;
     switch (*fmt) {
       case 'c': {
-        *str++ = (char) va_arg(ap, int);
+        c = (char) va_arg(ap, int);
+        func_op(str, c, func_num);
+        if (str != NULL) str++;
+        ret++;
         break;
       }
       case 's': {
         s = va_arg(ap, char*);
         len = strlen(s);
-        for (int i = 0; i < len; ++i) *str++ = *s++;
+        for (int i = 0; i < len; ++i) {
+          func_op(str, *(s+i), func_num);
+          if (str != NULL) str++;
+          ret++;
+        }
         break;
       }
       case 'i':
@@ -37,13 +53,28 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
           d_num[len++] = '0' + d%10;
           d /= 10;
         }
-        for (int i = 0; i < len; ++i) *str++ = d_num[len-i-1];
+        for (int i = 0; i < len; ++i) {
+          func_op(str, *(d_num+len-i-1), func_num);
+          if (str != NULL) str++;
+          ret++;
+        }
         break;
       }
     }
   }
-  *str = '\0';
-  return str - out;
+  if (str != NULL) *str = '\0';
+  return ret;
+}
+
+int printf(const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  va_end(ap);
+  return fmtop(NULL, fmt, ap, Printf);
+}
+
+int vsprintf(char *out, const char *fmt, va_list ap) {
+  return fmtop(out, fmt, ap, Vsprintf);
 }
 
 int sprintf(char *out, const char *fmt, ...) {
