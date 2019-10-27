ce4f454f1629739e6b6776a13b09f2202333e325
tracer-ics2019 <tracer@njuics.org>
2019-10-27 18:57:25 +0800

    >  run 181860066 nmy Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  18:57:25 up  4:05,  1 user,  load average: 0.29, 0.33, 0.29 41e07bd242457836fd4a0b026cbb105d9a74bb2f

--- nexus-am/am/src/nemu-common/nemu-timer.c
+++ nexus-am/am/src/nemu-common/nemu-timer.c
@@ -1,13 +1,13 @@
 #include <am.h>
 #include <amdev.h>
 #include <nemu.h>
-
+uint32_t boot_time;
 size_t __am_timer_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_TIMER_UPTIME: {
       _DEV_TIMER_UPTIME_t *uptime = (_DEV_TIMER_UPTIME_t *)buf;
       uptime->hi = 0;
-      uptime->lo = inl(0x48);
+      uptime->lo = inl(RTC_ADDR) - boot_time;
       return sizeof(_DEV_TIMER_UPTIME_t);
     }
     case _DEVREG_TIMER_DATE: {
@@ -25,4 +25,5 @@ size_t __am_timer_read(uintptr_t reg, void *buf, size_t size) {
 }
 
 void __am_timer_init() {
+  boot_time = inl(RTC_ADDR);
 }
