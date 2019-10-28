8bf56cc39650d1a07ec49244511792b8b4220bb7
tracer-ics2019 <tracer@njuics.org>
2019-10-28 08:52:55 +0800

    >  run 181860066 nmy Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  08:52:55 up 17 min,  1 user,  load average: 0.35, 0.68, 0.58 ea36ffb97011c5b8b175f0907047796d9473c46d

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -4,39 +4,39 @@
 
 size_t strlen(const char *s) {
   int ret = 0;
-  while (*s++) ++ret;
+  //while (*s++) ++ret;
   return ret;
 }
 
 char *strcpy(char* dst,const char* src) {
   char* tmp = dst;
-  while (*src != '\0')
-    *dst++ = *src++;
+  //while (*src != '\0')
+  //  *dst++ = *src++;
   *dst = '\0';
   return tmp;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
   char* tmp = dst;
-  while (*src != '\0'&&n-- > 0)
-    *dst++ = *src++;
+  //while (*src != '\0'&&n-- > 0)
+  //  *dst++ = *src++;
   *dst = '\0';
   return tmp;
 }
 
 char* strcat(char* dst, const char* src) {
   char* tmp = dst;
-  while(*dst) dst++;
-  while (*src != '\0')
-    *dst++ = *src++;
+  //while(*dst) dst++;
+  //while (*src != '\0')
+  //  *dst++ = *src++;
   *dst = '\0';
   return tmp;
 }
 
 int strcmp(const char* s1, const char* s2) {
   int ret = 0;
-  while (!(ret = *(unsigned char*)s1 - *(unsigned char*)s2)&&*s2)
-    ++s1, ++s2;
+  //while (!(ret = *(unsigned char*)s1 - *(unsigned char*)s2)&&*s2)
+  //  ++s1, ++s2;
   if (ret < 0) return -1;
   else if (ret > 0) return 1;
   return 0;
@@ -44,27 +44,27 @@ int strcmp(const char* s1, const char* s2) {
 
 int strncmp(const char* s1, const char* s2, size_t n) {
   int ret = 0;
-  while (!(ret = *(unsigned char*)s1 - *(unsigned char*)s2)&&*s2&&n>0)
-    ++s1, ++s2, --n;
+  //while (!(ret = *(unsigned char*)s1 - *(unsigned char*)s2)&&*s2&&n>0)
+  //  ++s1, ++s2, --n;
   if (ret < 0) return -1;
   else if (ret > 0) return 1;
   return 0;
 }
 
 void* memset(void* v,int c,size_t n) {
-  unsigned char uc = c;
-  unsigned char *s;
-  for (s = v; n > 0; ++s, --n)
-    *s = uc;
+  //unsigned char uc = c;
+  //unsigned char *s;
+  //for (s = v; n > 0; ++s, --n)
+  //  *s = uc;
   return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
   unsigned char* dst = (unsigned char*)out;
-  unsigned char* src = (unsigned char*)in;
+  //unsigned char* src = (unsigned char*)in;
   unsigned char* tmp = dst;
-  while (n-- > 0)
-    *dst++ = *src++;
+  //while (n-- > 0)
+  //  *dst++ = *src++;
   return tmp;
 }
 
@@ -73,8 +73,8 @@ int memcmp(const void* s1, const void* s2, size_t n){
     return 0;
   unsigned char* v1 = (unsigned char*)s1;
   unsigned char* v2 = (unsigned char*)s2;
-  while (*v1 == *v2 && n > 0)
-    v1++, v2++, --n;
+  //while (*v1 == *v2 && n > 0)
+  //  v1++, v2++, --n;
   if (n == 0) return 0; 
   return *v1 - *v2;
 }
