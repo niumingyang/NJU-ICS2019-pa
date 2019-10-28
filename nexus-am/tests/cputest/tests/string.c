98f3dd767c7422d7207c3ff6ca9eb11b71580aae
tracer-ics2019 <tracer@njuics.org>
2019-10-28 08:46:10 +0800

    >  compile 181860066 nmy Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  08:46:10 up 10 min,  1 user,  load average: 0.73, 0.71, 0.48 8fcf3fa4b69755b7fb19ad06329a75dcc796143f

--- nexus-am/tests/cputest/tests/string.c
+++ nexus-am/tests/cputest/tests/string.c
@@ -22,6 +22,6 @@ int main() {
 	nemu_assert(strcmp( strcat(strcpy(str, str1), s[3]), s[4]) == 0);
 	
 	nemu_assert(memcmp(memset(str, '#', 5), s[5], 5) == 0);
-
+printf("3e%s %c\n","abc", 'x');
 	return 0;
 }
