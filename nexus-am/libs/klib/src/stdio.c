eaee6609de4f08c232514df51aa13d4d798a4f5d
tracer-ics2019 <tracer@njuics.org>
2019-10-28 08:47:50 +0800

    >  run 181860066 nmy Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  08:47:50 up 12 min,  1 user,  load average: 0.29, 0.61, 0.47 9eed86f33f33ba1c2f616d56f886f00710c49cc

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -27,14 +27,6 @@ int fmtop(char *out, const char *fmt, va_list ap, int func_num) {
       continue;
     }
     fmt++;
-
-    /*if (*fmt=='c') {
-      c = (char) va_arg(ap, int);
-      func_op(str, c, func_num);
-      if (str != NULL) str++;
-      ret++;
-    }*/
-
     switch (*fmt) {
       case 's': {
         s = va_arg(ap, char*);
