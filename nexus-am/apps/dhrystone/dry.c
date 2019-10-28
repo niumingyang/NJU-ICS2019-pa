eed1d59afeb514db6099633805dda77ed8097ef7
tracer-ics2019 <tracer@njuics.org>
2019-10-28 09:34:00 +0800

    >  run 181860066 nmy Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  09:34:00 up 58 min,  1 user,  load average: 0.81, 0.55, 0.48 e302e37ec6d52b64865a211fcb80fa39984afc7

--- nexus-am/apps/dhrystone/dry.c
+++ nexus-am/apps/dhrystone/dry.c
@@ -356,7 +356,7 @@
 #define Start_Timer() Begin_Time = uptime()
 #define Stop_Timer()  End_Time   = uptime()
 
-#define NUMBER_OF_RUNS		1//500000 /* Default number of runs */
+#define NUMBER_OF_RUNS		5//00000 /* Default number of runs */
 #define PASS2
 
 #ifdef  NOSTRUCTASSIGN
