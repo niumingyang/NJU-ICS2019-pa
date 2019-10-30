b2cf4bd14bfa534928ca66488012ab8e5f343c4a
tracer-ics2019 <tracer@njuics.org>
2019-10-30 10:17:39 +0800

    >  compile 181860066 nmy Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  10:17:39 up  1:40,  1 user,  load average: 0.77, 0.87, 0.84 766ed92f22041a818a21142c58734800ae85e3fb

--- nemu/src/isa/x86/include/isa/rtl.h
+++ nemu/src/isa/x86/include/isa/rtl.h
@@ -42,6 +42,7 @@ static inline void rtl_is_sub_overflow(rtlreg_t* dest,
   // dest <- is_overflow(src1 - src2)
   rtl_msb(&t0, src1, width);
   rtl_msb(&t1, src2, width);
+  assert((t0==0&&(int32_t)*src1>=0)||(t0==1&&(int32_t)*src1<0));
   *dest = ((int32_t)*src1>=0&&(int32_t)*src2<0&&(int32_t)*res<0&&*src2!=0x80000000u)||
 	  ((int32_t)*src1<0&&(int32_t)*src2>=0&&(int32_t)*res>=0)||
 	  (*src2==0x80000000u&&(int32_t)*src1>=0);
