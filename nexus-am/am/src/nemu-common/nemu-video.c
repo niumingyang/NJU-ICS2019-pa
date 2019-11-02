e16dc85d8bf7256f38b756d2b17e2ee279f8f5e0
tracer-ics2019 <tracer@njuics.org>
2019-11-02 20:17:37 +0800

    >  run 181860066 nmy Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  20:17:37 up  1:08,  1 user,  load average: 0.55, 0.54, 0.49 3414c005fd515733bbe8c7a0b8f6bb02f7ac84c

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -7,6 +7,10 @@ uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
 static int W = 400;
 static int H = 300;
 
+static inline int min(int x, int y) {
+  return (x < y) ? x : y;
+}
+
 size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
@@ -26,9 +30,10 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
       int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
       uint32_t *pixels = ctl->pixels;
-      for (int j = 0; j < h && y + j < H; j ++) {
+      int h_min = min(h, H - y), w_min = min(w, W - x);
+      for (int j = 0; j < h_min; ++j) {
         int tmp = (y + j) * W + x;
-        for (int i = 0; i < w && x + i < W; ++i)
+        for (int i = 0; i < w_min; ++i)
           fb[tmp + i] = pixels[i];
         pixels += w;
       }
