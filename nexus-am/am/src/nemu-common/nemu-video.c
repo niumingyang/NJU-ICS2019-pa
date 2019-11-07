06dcc1a3849b7fc3a295a11883e1ee7c45969dd5
tracer-ics2019 <tracer@njuics.org>
2019-11-07 17:02:26 +0800

    >  run 181860066 nmy Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  17:02:26 up  8:06,  1 user,  load average: 1.75, 1.42, 0.91 c9d8aef75252646b59784a946b669bf32e080305

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -4,10 +4,8 @@
 #include <klib.h>
 
 uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-/*static int W = 400;
-static int H = 300;*/
-#define W screen_width()
-#define H screen_height()
+static int W = 400;
+static int H = 300;
 
 size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
