1b322d502446b9657add938e36928913860c10f8
tracer-ics2019 <tracer@njuics.org>
2019-11-17 21:45:06 +0800

    >  run 181860066 nmy Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  21:45:06 up  7:07,  1 user,  load average: 0.39, 0.41, 0.28 33225924a898ba64b44e465f36c42094721e500

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -13,11 +13,17 @@ size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t get_ramdisk_size();
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
+  Elf_Ehdr Ehdr_info;
   Elf_Phdr Phdr_info;
-  ramdisk_read(&Phdr_info, sizeof(Elf_Ehdr), sizeof(Elf_Phdr));
-  ramdisk_read((uintptr_t *)Phdr_info.p_vaddr, Phdr_info.p_offset, Phdr_info.p_filesz);
-  memset((uintptr_t *)(Phdr_info.p_vaddr + Phdr_info.p_filesz), 0, Phdr_info.p_memsz - Phdr_info.p_filesz);
-  return Phdr_info.p_vaddr;
+  ramdisk_read(&Ehdr_info, 0, sizeof(Elf_Ehdr));
+  size_t offset = Ehdr_info.e_phoff;
+  for (int i = 0; i < Ehdr_info.e_phnum; ++i) {
+    ramdisk_read(&Phdr_info, offset, Ehdr_info.e_phentsize);
+    ramdisk_read((uintptr_t *)Phdr_info.p_vaddr, Phdr_info.p_offset, Phdr_info.p_filesz);
+    memset((uintptr_t *)(Phdr_info.p_vaddr + Phdr_info.p_filesz), 0, Phdr_info.p_memsz - Phdr_info.p_filesz);
+    offset += Ehdr_info.e_phentsize;
+  }
+  return Ehdr_info.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) { 
