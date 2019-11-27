3391e0c39bdfe28eeb62c735fa6003df133c1a9d
tracer-ics2019 <tracer@njuics.org>
2019-11-27 11:29:13 +0800

    >  run 181860066 nmy Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  11:29:13 up 23 min,  1 user,  load average: 1.13, 1.15, 0.99 e8a957eaa00e2791e7f92ef42e3ce4495258b87a

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -32,20 +32,19 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 
 static char dispinfo[128] __attribute__((used)) = {};
 
-size_t dispinfo_read(void *buf, size_t offset, size_t len) {assert(0);
+size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   memcpy(buf, dispinfo + offset, len);
   return len;
 }
 
-size_t fb_write(const void *buf, size_t offset, size_t len) {assert(0);
-  int W = screen_width(), H = screen_height();
-  int x = offset % W, y = offset / W + 1;
-  uint32_t *pixels = (uint32_t *)buf;
-  draw_rect(pixels, x, y, W, H);
+size_t fb_write(const void *buf, size_t offset, size_t len) {
+  int W = screen_width(), oft = offset / 4;
+  int y = oft % W, x = oft / W;
+  draw_rect((uint32_t *)buf, x, y, len/4, 1);
   return 0;
 }
 
-size_t fbsync_write(const void *buf, size_t offset, size_t len) {assert(0);
+size_t fbsync_write(const void *buf, size_t offset, size_t len) {
   draw_sync();
   return len;
 }
@@ -53,7 +52,7 @@ size_t fbsync_write(const void *buf, size_t offset, size_t len) {assert(0);
 void init_device() {
   Log("Initializing devices...");
   _ioe_init();
-  sprintf(dispinfo, "WIDTH:%d\nHEIGNT:%d", screen_width(), screen_height);
+  sprintf(dispinfo, "WIDTH:%d\nHEIGNT:%d\n", screen_width(), screen_height);
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
 }
