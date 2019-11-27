9d6d9c764a0e5b92324a79014996244a7ce15aba
tracer-ics2019 <tracer@njuics.org>
2019-11-27 21:15:13 +0800

    >  run 181860066 nmy Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  21:15:13 up 10:09,  1 user,  load average: 0.85, 0.85, 0.73 9e5eabc2e75c58eb1f1eb61dfe25c65d16bcb173

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -74,8 +74,8 @@ void *_sbrk(intptr_t increment) {
 }
 
 int _read(int fd, void *buf, size_t count) {
-  assert(0);
-  return _syscall_(SYS_read, fd, (intptr_t)buf, count);
+  int ret = _syscall_(SYS_read, fd, (intptr_t)buf, count);
+  return ret;
 }
 
 int _close(int fd) {
