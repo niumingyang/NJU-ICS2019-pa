c21ce918d16d58f9a517f9937179c8eae0031488
tracer-ics2019 <tracer@njuics.org>
2019-11-28 11:17:23 +0800

    >  run 181860066 nmy Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  11:17:23 up 59 min,  1 user,  load average: 1.07, 1.30, 1.39 8832867590a19719257f62bc5bbbca94af3530d7

--- navy-apps/libs/libndl/src/bmp.c
+++ navy-apps/libs/libndl/src/bmp.c
@@ -27,7 +27,7 @@ int NDL_LoadBitmap(NDL_Bitmap *bmp, const char *filename) {
   w = h = 0;
   if (!(fp = fopen(filename, "r"))) return -1;
 printf("%d*******", (int)fp);
-  struct BitmapHeader hdr;assert(0);
+  struct BitmapHeader hdr;//assert(0);
   assert(sizeof(hdr) == 54);
   assert(1 == fread(&hdr, sizeof(struct BitmapHeader), 1, fp));
   
