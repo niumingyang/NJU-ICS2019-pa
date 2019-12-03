6e02ab4c4cc443743f57a6f6c4f4fa5132e5f385
tracer-ics2019 <tracer@njuics.org>
2019-12-03 16:08:26 +0800

    >  run 181860066 nmy Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  16:08:26 up 43 min,  1 user,  load average: 1.91, 1.48, 1.23 b2190fa6ef08987aa59170b9b36d95ec72f57f4

--- navy-apps/apps/init/init.cpp
+++ navy-apps/apps/init/init.cpp
@@ -110,7 +110,7 @@ int main(int argc, char *argv[], char *envp[]) {
       exec_argv[0] = item->bin;
       exec_argv[1] = item->arg1;
       exec_argv[2] = NULL;
-      clear_display();assert(0);
+      clear_display();
       execve(exec_argv[0], (char**)exec_argv, (char**)envp);
       fprintf(stderr, "\033[31m[ERROR]\033[0m Exec %s failed.\n\n", exec_argv[0]);
     } else {
--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -87,8 +87,7 @@ off_t _lseek(int fd, off_t offset, int whence) {
 #include <assert.h>
 #include <stdio.h>
 int _execve(const char *fname, char * const argv[], char *const envp[]) {
-  printf("%s*****\n",fname);
-  //assert(0);
+  assert(fname!=NULL);
   return _syscall_(SYS_execve, (intptr_t)fname, 0, 0);
 }
 
