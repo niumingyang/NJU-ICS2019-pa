5bfc55860219e10ec8693bd52d146df00f533bc0
tracer-ics2019 <tracer@njuics.org>
2019-12-18 20:27:27 +0800

    >  run 181860066 nmy Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  20:27:27 up  1:40,  1 user,  load average: 0.70, 0.40, 0.31 aefb11c24659de59a83d336d56ca87a58550206

--- nexus-am/am/src/x86/nemu/cte.c
+++ nexus-am/am/src/x86/nemu/cte.c
@@ -51,7 +51,7 @@ int _cte_init(_Context*(*handler)(_Event, _Context*)) {
 
 _Context *_kcontext(_Area stack, void (*entry)(void *), void *arg) {
   _Context *c = (_Context*)(stack.end) - 1;
-  *(_Context**)(stack.start) = c;
+  //*(_Context**)(stack.start) = c;
   c->cs = 8;
   c->pc = (uintptr_t)entry;
   return c;
