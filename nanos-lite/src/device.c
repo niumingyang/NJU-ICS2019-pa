34b42f36bcd9c0680cdc02f43f0215f8316ae1e1
tracer-ics2019 <tracer@njuics.org>
2019-12-18 20:51:16 +0800

    >  run 181860066 nmy Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  20:51:16 up  2:04,  1 user,  load average: 0.35, 0.45, 0.38 1909f8c6bf2a25768e8fd77dcae1326f3abeeb7

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -2,6 +2,7 @@
 #include <amdev.h>
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
+  _yield();
   for (int i = 0; i < len; ++i)
     _putc(((const char *)buf)[i]);
   return len;
@@ -16,6 +17,7 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t events_read(void *buf, size_t offset, size_t len) {
+  _yield();
   int key = read_key();
   bool keydown = false;
   if (key & 0x8000) {
@@ -42,6 +44,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
+  _yield();
   offset /= 4;
   int x = offset % W, y = offset / W;
   draw_rect((uint32_t *)buf, x, y, len/4, 1);
