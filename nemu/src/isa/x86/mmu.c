1bff207fd26d8253f73dd34a8865145ffe22c94b
tracer-ics2019 <tracer@njuics.org>
2019-12-20 18:21:57 +0800

    >  compile 181860066 nmy Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  18:21:57 up 1 day, 23:35,  1 user,  load average: 0.85, 0.51, 0.34 a59259b334e0f7d1387a438cb3097acd5c50d6ed

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -1,9 +1,39 @@
 #include "nemu.h"
 
+paddr_t page_translate(vaddr_t addr){
+  if(cpu.cr0.paging){
+    paddr_t dir = (addr >> 22) & 0x3ff;
+    paddr_t page = (addr >> 12) & 0x3ff;
+    paddr_t offset = addr & 0xfff;
+    uint32_t pdb = cpu.cr3.page_directory_base;
+    
+    uint32_t pt = paddr_read((pdb << 12) + (dir << 2), 4);
+    assert(pt & 1);
+
+    uint32_t pf = paddr_read((pt & 0xfffff000) + (page << 2), 4);
+    assert(pf & 1);
+
+    return (pf & 0xfffff000) + offset;
+  }
+  return addr;
+}
+
 uint32_t isa_vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  if((addr & 0xfff) + len > 0x1000){
+    assert(0);
+  }
+  else {
+    paddr_t paddr = page_translate(addr);
+    return paddr_read(paddr, len);
+  }
 }
 
 void isa_vaddr_write(vaddr_t addr, uint32_t data, int len) {
-  paddr_write(addr, data, len);
+  if((addr & 0xfff) + len > 0x1000){
+    assert(0);
+  }
+  else {
+    paddr_t paddr = page_translate(addr);
+    paddr_write(paddr, data, len);
+  }
 }
--- nexus-am/am/src/x86/nemu/vme.c
+++ nexus-am/am/src/x86/nemu/vme.c
@@ -65,6 +65,8 @@ int _protect(_AddressSpace *as) {
 }
 
 void _unprotect(_AddressSpace *as) {
+  pgfree_usr(as->ptr);
+  as->ptr = as->area.start = as->area.end = NULL;
 }
 
 static _AddressSpace *cur_as = NULL;
@@ -80,12 +82,23 @@ void __am_switch(_Context *c) {
 }
 
 int _map(_AddressSpace *as, void *va, void *pa, int prot) {
+  uint32_t* ptr = (uint32_t*)as->ptr;
+  uint32_t shift = (uintptr_t)va >> 22;
+  uintptr_t tr = ptr[shift];
+
+  if (tr == kpdirs[shift]) {
+    PTE *uptable = (PTE*)(pgalloc_usr(1));
+    ptr[shift] = (uintptr_t)uptable | PTE_P;
+  }
+
+  shift = (((uintptr_t)va) & 0x003ff000) >> 12;
+  uint32_t* pgr = (uint32_t*)(tr & 0xfffff000);
+  pgr[shift] = (uintptr_t)pa | PTE_P;
   return 0;
 }
 
 _Context *_ucontext(_AddressSpace *as, _Area ustack, _Area kstack, void *entry, void *args) {
   _Context *c = (_Context*)(ustack.end) - sizeof(_Context) - 4*sizeof(uintptr_t);
-  //c->eflags = 0x2 | (1 << 9);
   c->cs = 8;
   c->pc = (uintptr_t)entry;
   return c;
