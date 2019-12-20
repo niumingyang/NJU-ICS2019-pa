f89e32ba2c253e4285db7dc1fd4c1ebad716d18f
tracer-ics2019 <tracer@njuics.org>
2019-12-20 10:37:56 +0800

    >  run 181860066 nmy Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  10:37:56 up 1 day, 15:51,  1 user,  load average: 1.06, 1.02, 0.85 ee23c475e2fc8b236566faa1a1b42833d75face1

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -40,7 +40,6 @@ _Context* schedule(_Context *prev) {
   current->cp = prev;
 
   //current = &pcb[0];
-  current = &pcb[1];
-  //current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
   return current->cp;
 }
