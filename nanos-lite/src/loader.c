d3930e46bfd647c8b7d5adb322abb7eea6729c95
tracer-ics2019 <tracer@njuics.org>
2019-12-21 13:54:22 +0800

    >  run 181860066 nmy Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  13:54:22 up 2 days, 19:07,  1 user,  load average: 0.49, 0.40, 0.33 756e449b4d0dafb5a3720adc54b5e6fcff34ff4

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -42,75 +42,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   fs_close(fd);
   return Ehdr_info.e_entry;
 }
-/*#define PAGE_SIZE 4096
-static uintptr_t loader(PCB *pcb, const char *filename) {
-  int fd = fs_open(filename, 0, 0);
-  Elf_Ehdr Elf_Header;
-  fs_read(fd, (void*)&Elf_Header, sizeof(Elf_Ehdr));
 
-  Elf_Phdr Elf_Program_Header;
-  for (int i=0;i<Elf_Header.e_phnum;i++) {
-    fs_lseek(fd, Elf_Header.e_phoff + i*Elf_Header.e_phentsize, SEEK_SET);
-    fs_read(fd, (void*)&Elf_Program_Header, Elf_Header.e_phentsize);
-    
-    
-    if (Elf_Program_Header.p_type == PT_LOAD) 
-    {
-      
-      uint32_t un_size = Elf_Program_Header.p_filesz; //需要写入的字节数
-      uint32_t v_addr  = Elf_Program_Header.p_vaddr;
-      void* p_addr;
-
-      fs_lseek(fd, Elf_Program_Header.p_offset, SEEK_SET);
-      while (1) {
-        p_addr = new_page(1); //申请一页空闲的物理页
-        _map(&(pcb->as), (void*)v_addr, p_addr, 0);
-
-        if (un_size > PAGE_SIZE)
-          fs_read(fd, p_addr, PAGE_SIZE);
-        else{
-          fs_read(fd, p_addr, un_size);
-          // pcb->max_brk = v_addr + un_size;
-          break;
-        }
-
-        un_size -= PAGE_SIZE;
-        v_addr  += PAGE_SIZE;
-      }
-
-      if (Elf_Program_Header.p_memsz > Elf_Program_Header.p_filesz) {
-        size_t zero_size = Elf_Program_Header.p_memsz - Elf_Program_Header.p_filesz;
-        
-        if (zero_size <= PAGE_SIZE - un_size) {
-          memset((void*)((uint32_t)p_addr + un_size*4), 0, zero_size);
-        } else {
-          memset((void*)((uint32_t)p_addr + un_size*4), 0, PAGE_SIZE - un_size);
-          zero_size -= (PAGE_SIZE - un_size);
-          v_addr    += PAGE_SIZE;
-
-          while (1) {
-            p_addr = new_page(1);
-            _map(&(pcb->as), (void*)v_addr, p_addr, 0);
-
-            if (zero_size>PAGE_SIZE) 
-              memset(p_addr, 0, PAGE_SIZE);
-            else {
-              memset(p_addr, 0, zero_size);
-              // pcb->max_brk = v_addr + zero_size;
-              break;
-            }
-
-            zero_size -= PAGE_SIZE;
-            v_addr    += PAGE_SIZE;
-          }
-        }
-      }
-      pcb->max_brk = v_addr+PAGE_SIZE;
-    }
-  }
-  fs_close(fd);
-  return Elf_Header.e_entry;
-}*/
 void naive_uload(PCB *pcb, const char *filename) { 
   uintptr_t entry = loader(pcb, filename);
   Log("Jump to entry = %x", entry);
