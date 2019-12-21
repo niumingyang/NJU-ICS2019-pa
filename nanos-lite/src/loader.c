273be81aa1b67964da345f2353e5d4e97faaa057
tracer-ics2019 <tracer@njuics.org>
2019-12-21 14:40:16 +0800

    >  run 181860066 nmy Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  14:40:16 up 2 days, 19:53,  1 user,  load average: 0.57, 0.57, 0.59 dbf653ad8310cb5aaff64100ba6021621e6ac65d

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -16,96 +16,65 @@ ssize_t fs_write(int fd, const void *buf, size_t count);
 int fs_close(int fd);
 off_t fs_lseek(int fd, off_t offset, int whence);
 
-/*static uintptr_t loader(PCB *pcb, const char *filename) {
+static uintptr_t loader(PCB *pcb, const char *filename) {
   int fd = fs_open(filename, 0, 0);
   Elf_Ehdr Ehdr_info;
   Elf_Phdr Phdr_info;
-  size_t rd_offset = sizeof(Elf_Ehdr);
   fs_read(fd, &Ehdr_info, sizeof(Elf_Ehdr));
   for (int i = 0; i < Ehdr_info.e_phnum; ++i) {
+    fs_lseek(fd, Ehdr_info.e_phoff + i*Ehdr_info.e_phentsize, SEEK_SET);
     fs_read(fd, &Phdr_info, Ehdr_info.e_phentsize);
-    fs_lseek(fd, Phdr_info.p_offset, SEEK_SET);
-    size_t size = Phdr_info.p_filesz;
-    size_t page_cnt = (size + PGSIZE - 1) / PGSIZE;
-    void* pa;
-    void* va = (uintptr_t *)Phdr_info.p_vaddr;
-    for(int i = 0; i < page_cnt; ++i){
-      pa = new_page(1);
-      _map(&pcb->as, va, pa, 0);
-      fs_read(fd, pa, (((size - i * PGSIZE) < PGSIZE) ? (size - i * PGSIZE) : PGSIZE));
-      va += PGSIZE;
-    }
-    //memset((uintptr_t *)(Phdr_info.p_vaddr + Phdr_info.p_filesz), 0, Phdr_info.p_memsz - Phdr_info.p_filesz);
-    rd_offset += Ehdr_info.e_phentsize;
-    fs_lseek(fd, rd_offset, SEEK_SET);
-  }
-  fs_close(fd);
-  return Ehdr_info.e_entry;
-}*/
-static uintptr_t loader(PCB *pcb, const char *filename) {
-  int fd = fs_open(filename, 0, 0);
-  Elf_Ehdr Elf_Header;
-  fs_read(fd, (void*)&Elf_Header, sizeof(Elf_Ehdr));
+    if (Phdr_info.p_type == PT_LOAD) {
+      uint32_t un_size = Phdr_info.p_filesz;
+      uint32_t va  = Phdr_info.p_vaddr;
+      void* pa;
 
-  Elf_Phdr Elf_Program_Header;
-  for (int i=0;i<Elf_Header.e_phnum;i++) {
-    fs_lseek(fd, Elf_Header.e_phoff + i*Elf_Header.e_phentsize, SEEK_SET);
-    fs_read(fd, (void*)&Elf_Program_Header, Elf_Header.e_phentsize);
-    
-    
-    if (Elf_Program_Header.p_type == PT_LOAD) 
-    {
-      
-      uint32_t un_size = Elf_Program_Header.p_filesz;
-      uint32_t v_addr  = Elf_Program_Header.p_vaddr;
-      void* p_addr;
-
-      fs_lseek(fd, Elf_Program_Header.p_offset, SEEK_SET);
+      fs_lseek(fd, Phdr_info.p_offset, SEEK_SET);
       while (1) {
-        p_addr = new_page(1);
-        _map(&(pcb->as), (void*)v_addr, p_addr, 0);
+        pa = new_page(1);
+        _map(&(pcb->as), (void*)va, pa, 0);
 
         if (un_size > PGSIZE)
-          fs_read(fd, p_addr, PGSIZE);
+          fs_read(fd, pa, PGSIZE);
         else{
-          fs_read(fd, p_addr, un_size);
+          fs_read(fd, pa, un_size);
           break;
         }
         un_size -= PGSIZE;
-        v_addr  += PGSIZE;
+        va  += PGSIZE;
       }
 
-      if (Elf_Program_Header.p_memsz > Elf_Program_Header.p_filesz) {
-        size_t zero_size = Elf_Program_Header.p_memsz - Elf_Program_Header.p_filesz;
+      if (Phdr_info.p_memsz > Phdr_info.p_filesz) {
+        size_t zero_size = Phdr_info.p_memsz - Phdr_info.p_filesz;
         
         if (zero_size <= PGSIZE - un_size) {
-          memset((void*)((uint32_t)p_addr + un_size*4), 0, zero_size);
+          memset((void*)((uint32_t)pa + un_size*4), 0, zero_size);
         } else {
-          memset((void*)((uint32_t)p_addr + un_size*4), 0, PGSIZE - un_size);
+          memset((void*)((uint32_t)pa + un_size*4), 0, PGSIZE - un_size);
           zero_size -= (PGSIZE - un_size);
-          v_addr    += PGSIZE;
+          va    += PGSIZE;
 
           while (1) {
-            p_addr = new_page(1);
-            _map(&(pcb->as), (void*)v_addr, p_addr, 0);
+            pa = new_page(1);
+            _map(&(pcb->as), (void*)va, pa, 0);
 
             if (zero_size>PGSIZE) 
-              memset(p_addr, 0, PGSIZE);
+              memset(pa, 0, PGSIZE);
             else {
-              memset(p_addr, 0, zero_size);
+              memset(pa, 0, zero_size);
               break;
             }
 
             zero_size -= PGSIZE;
-            v_addr    += PGSIZE;
+            va    += PGSIZE;
           }
         }
       }
-      pcb->max_brk = v_addr+PGSIZE;
+      pcb->max_brk = va + PGSIZE;
     }
   }
   fs_close(fd);
-  return Elf_Header.e_entry;
+  return Ehdr_info.e_entry;
 }
 void naive_uload(PCB *pcb, const char *filename) { 
   uintptr_t entry = loader(pcb, filename);
