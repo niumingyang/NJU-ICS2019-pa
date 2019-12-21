70dd8e51eb994e07241bc7f0e6b38b18a218463e
tracer-ics2019 <tracer@njuics.org>
2019-12-21 14:31:04 +0800

    >  run 181860066 nmy Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  14:31:04 up 2 days, 19:44,  1 user,  load average: 0.95, 0.85, 0.65 e9166d435f1b10a718c88a857aaa0aa3a73ae35c

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -42,7 +42,6 @@ off_t fs_lseek(int fd, off_t offset, int whence);
   fs_close(fd);
   return Ehdr_info.e_entry;
 }*/
-#define PAGE_SIZE 4096
 static uintptr_t loader(PCB *pcb, const char *filename) {
   int fd = fs_open(filename, 0, 0);
   Elf_Ehdr Elf_Header;
@@ -57,29 +56,52 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
     if (Elf_Program_Header.p_type == PT_LOAD) 
     {
       
-      uint32_t un_size = Elf_Program_Header.p_filesz; //需要写入的字节数
+      uint32_t un_size = Elf_Program_Header.p_filesz;
       uint32_t v_addr  = Elf_Program_Header.p_vaddr;
       void* p_addr;
 
       fs_lseek(fd, Elf_Program_Header.p_offset, SEEK_SET);
       while (1) {
-        p_addr = new_page(1); //申请一页空闲的物理页
+        p_addr = new_page(1);
         _map(&(pcb->as), (void*)v_addr, p_addr, 0);
 
-        if (un_size > PAGE_SIZE)
-          fs_read(fd, p_addr, PAGE_SIZE);
+        if (un_size > PGSIZE)
+          fs_read(fd, p_addr, PGSIZE);
         else{
           fs_read(fd, p_addr, un_size);
-          // pcb->max_brk = v_addr + un_size;
           break;
         }
-
-        un_size -= PAGE_SIZE;
-        v_addr  += PAGE_SIZE;
+        un_size -= PGSIZE;
+        v_addr  += PGSIZE;
       }
 
-  
-      pcb->max_brk = v_addr+PAGE_SIZE;
+      if (Elf_Program_Header.p_memsz > Elf_Program_Header.p_filesz) {
+        size_t zero_size = Elf_Program_Header.p_memsz - Elf_Program_Header.p_filesz;
+        
+        if (zero_size <= PGSIZE - un_size) {
+          memset((void*)((uint32_t)p_addr + un_size*4), 0, zero_size);
+        } else {
+          memset((void*)((uint32_t)p_addr + un_size*4), 0, PGSIZE - un_size);
+          zero_size -= (PGSIZE - un_size);
+          v_addr    += PGSIZE;
+
+          while (1) {
+            p_addr = new_page(1);
+            _map(&(pcb->as), (void*)v_addr, p_addr, 0);
+
+            if (zero_size>PGSIZE) 
+              memset(p_addr, 0, PGSIZE);
+            else {
+              memset(p_addr, 0, zero_size);
+              break;
+            }
+
+            zero_size -= PGSIZE;
+            v_addr    += PGSIZE;
+          }
+        }
+      }
+      pcb->max_brk = v_addr+PGSIZE;
     }
   }
   fs_close(fd);
